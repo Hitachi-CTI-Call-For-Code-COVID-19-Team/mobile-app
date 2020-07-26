e5b9f1593abcf75e4cd15ab5bba64e5d2a6b2d98
Watanabe Kentaro <kentaro.watanabe.dc@hitachi.com>
2020-07-26 21:12:20 +0900

    update

--- swift/Pods/Target Support Files/Pods-MobileAppwithPushNotificationsC4CCOVID19Tests/Pods-MobileAppwithPushNotificationsC4CCOVID19Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileAppwithPushNotificationsC4CCOVID19Tests : NSObject
-@end
-@implementation PodsDummy_Pods_MobileAppwithPushNotificationsC4CCOVID19Tests
-@end
--- swift/Pods/Target Support Files/Pods-MobileAppwithPushNotificationsC4CCOVID19UITests/Pods-MobileAppwithPushNotificationsC4CCOVID19UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileAppwithPushNotificationsC4CCOVID19UITests : NSObject
-@end
-@implementation PodsDummy_Pods_MobileAppwithPushNotificationsC4CCOVID19UITests
-@end
